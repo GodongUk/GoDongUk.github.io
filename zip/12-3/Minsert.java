32bbcfd95807acae0a0adad5f0c1dc3e3100d631
GodongUk <80384549+GodongUk@users.noreply.github.com>
2021-12-04 09:34:20 +0900

    Update Minsert.java

--- zip/12-3/Minsert.java
+++ zip/12-3/Minsert.java
@@ -10,14 +10,10 @@ public class Minsert {
         conn=DriverManage.getConnection("jdbc:postgresql://localhost:5432/mydb","studx","gkrod123A!");
       String sql="INSERT INTO badge (stud_id, stud_passwd, stud_name, stud_email, stud_gender) VALUES
         (?,?,?,?,?)";
-        
-      if(idx=="all") {
-        pstmt = conn.prepareStatement("DELETE FROM badge");
-      }else{
-         pstmt = conn.prepareStatement("DELETE FROM badge WHERE stud_id=?");
-        pstmt.setString(1,stud_id);
-      }
-      pstmt.executeUpdate();
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, "stud_id"); pstmt.setString(2,"stud_passwd"); pstmt.setSting(3,"stud_name");
+      pstmt.setString(4, "stud_email");pstmt.setString(5,"stud_gender");
+      pstm.executeUpdate();
       flag=true;
     } catch(Exception e) {e.printStackTrace();
                          } finally {
