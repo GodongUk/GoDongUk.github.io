3b7158c2d4965d5039659b8a8d3a2b6a6fdc6371
GodongUk <80384549+GodongUk@users.noreply.github.com>
2021-12-04 09:31:43 +0900

    Update Minsert.java

--- zip/12-3/Minsert.java
+++ zip/12-3/Minsert.java
@@ -2,11 +2,15 @@ package db.test;
 import java.sql.*;
 public class Minsert {
   private Connection conn=null;
-  public boolean minsert(String idx, String stud_id) throws SQLException {
+  public boolean minsert(String stud_id, String stud_passwd, String stud_name, String stud_email, String 
+                         stud_gender) throws SQLException {
     boolean flag = false;
     PreparedStatement pstmt=null;
     try{
         conn=DriverManage.getConnection("jdbc:postgresql://localhost:5432/mydb","studx","gkrod123A!");
+      String sql="INSERT INTO badge (stud_id, stud_passwd, stud_name, stud_email, stud_gender) VALUES
+        (?,?,?,?,?)";
+        
       if(idx=="all") {
         pstmt = conn.prepareStatement("DELETE FROM badge");
       }else{
