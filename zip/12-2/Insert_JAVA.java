7605fe5eceb8ce39ad16a4f35b564fc333fb40a5
GodongUk <80384549+GodongUk@users.noreply.github.com>
2021-12-04 09:22:52 +0900

    Update Insert_JAVA.java

--- zip/12-2/Insert_JAVA.java
+++ zip/12-2/Insert_JAVA.java
@@ -9,4 +9,18 @@ public class Insert_Java {
     } catch(ClassNotFoundException e) {.printStackTrace(); }
     PreparedStatement pstmt=null;
     try{
-      conn=DriverManage.getConnection("jdbc:postgresql://localhost:5432/
+      conn=DriverManage.getConnection("jdbc:postgresql://localhost:5432/mydb","studx","gkrod123A!");
+      String sql="INSERT INTO badge (stud_id,stud_passwd,stud_name,stud_maile,stud_gender) VALUES
+        (?,?,?,?,?)";
+        pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, "hdbyun"); pstmt.setString(2,"4321"); pstmt.setSting(3,"변학도");
+      pstmt.setString(4, "hdbyun@abc.com");pstmt.setString(5,"남자");
+      int cnt = pstm.executeUpdate();
+      flag=true;
+      System.out.println(flag+" "+cnt);
+    } catch(Exception e) {e.printStackTrace();
+                         } finally {
+      try{ if(conn !=null)conn.close(); if(pstmt !=null) pstmt.close();
+         } catch(Exception e) {e.printStackTrace(); }
+    }}}
+
